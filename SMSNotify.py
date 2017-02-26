ff8a42d6923af2a4ac3353c346e0f2390cf13eb1
Ben Calvert <linuxg33k76@gmail.com>
2017-02-25 20:25:26 -0700

    Fixed twilioSMS class isslue in SMSNotify.py

--- SMSNotify.py
+++ SMSNotify.py
@@ -2,6 +2,7 @@
 #Author:  Ben Calvert
 #Date: August 7, 2016 v1.0.0
 #Revision: August 21, 2016 v1.0.1
+#Revision: August 21, 2016 v1.1.0 - Added Security for TwilioSMS via /etc/piviteye/twilio.conf file
 
 #This program sends out an SMS message for motion detected by webcam
 
@@ -12,14 +13,20 @@ import glob
 import twclass
 
 def main():
-    
+
     path = '/mnt/usb/video/'
     latest_video = max(glob.iglob(os.path.join(path, '*.[Aa][Vv][Ii]')), key=os.path.getctime)
     msg = 'Motion Detected!  Recording has been made: ' + latest_video
-    #Instantiate the class
-    tw = twclass.TwilioSMS()
+
+    # Load Twilio Configuration VALUES
+    with open ('/etc/piviteye/twilio.conf') as data_file:
+        data = json.load(data_file)
+
+    # New TwilioSMS instance
+    tw = twclass.TwilioSMS(data['tw_account'],data['tw_token'],data['tw_receiver'],data['tw_sender'])
+
     #Call send_message method
     tw.send_message(msg)
-    
+
 if __name__ == "__main__":
     main()
