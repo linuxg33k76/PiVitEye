ddf3800f8e6f0fd98087c4688e6c68a4f08896a1
Ben Calvert <linuxg33k76@gmail.com>
2017-03-05 10:01:50 -0700

    changed to uikit and added js button functionality to index.html

--- webapp/app.py
+++ webapp/app.py
@@ -24,7 +24,7 @@ def log():
         last_modified_date = datetime.datetime.fromtimestamp(mtime)
         mdate.append(last_modified_date)
 
-    return render_template('log.html', files=files, directory=basedir, modified=modified)
+    return render_template('log.html', files=files, directory=basedir, modified=mdate)
 
 
 @app.route('/videos')
@@ -60,6 +60,40 @@ def stream():
         return render_template('nolivefeed.html')
     else:
         return render_template('livefeed.html')
+
+@app.route('/pcap/start', methods=['POST'])
+def pcap_start():
+    cmd = os.popen('sudo tshark -i eth0 -a duration:30 -b filesize:2048 -w /mnt/usb/pcap/output.pcap -F pcap')
+    cmd.read()
+    cmd.close()
+    msg = 'User initiated packet capture is complete!'
+    return render_template('output.html', msg=msg)
+
+@app.route('/pcap/clear', methods=['POST'])
+def pcap_clear():
+    cmd = os.popen('sudo rm -f /mnt/usb/pcap/*.pcap')
+    cmd.read()
+    cmd.close()
+    msg = 'User initiated DELETION of PCAP files is complete!'
+    return render_template('output.html', msg=msg)
+
+@app.route('/motion/start', methods=['POST'])
+def motion_start():
+    cmd = os.popen('sudo service motion start')
+    cmd.read()
+    cmd.close()
+    msg = 'User initiated Start of Motion Service files is complete!'
+    return render_template('output.html', msg=msg)
+
+@app.route('/motion/stop', methods=['POST'])
+def motion_stop():
+    cmd = os.popen('sudo service motion stop')
+    cmd.read()
+    cmd.close()
+    msg = 'User initiated Stop of Motion Service is complete!'
+    return render_template('output.html', msg=msg)
+
+
 
 
 if __name__ == '__main__':
